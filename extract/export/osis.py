1c7022df58dbbdbc59a4b2d5ce96cf0ab8302857
karmek-k <33288445+karmek-k@users.noreply.github.com>
2025-11-08 19:58:29 +0100

    feat(export): add osis template

---
+++ extract/export/osis.py
@@ -0,0 +1,21 @@
+template = '''<?xml version='1.0' encoding='UTF-8'?>
+<osis xsi:schemaLocation='http://www.bibletechnologies.net/2003/OSIS/namespace http://www.bibletechnologies.net/osisCore.2.1.1.xsd' xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance' xmlns='http://www.bibletechnologies.net/2003/OSIS/namespace'>
+  <osisText osisRefWork='Bible' osisIDWork='BWujka' xml:lang='pl'>
+    <header>
+      <work osisWork='DOUR'>
+        <title>Biblia Jakuba Wujka</title>
+        <contributor>Bartosz Gleń</contributor>
+        <date></date>
+        <description>Biblia Jakuba Wujka wydana przez Brytyjskie i Zagraniczne Towarzystwo Biblijne w 1923 roku. Kanon protestancki, bez ksiąg deuterokanonicznych.</description>
+        <publisher>Bartosz Gleń</publisher>
+        <type type='OSIS'>Bible</type>
+        <identifier type='OSIS'>BWujka</identifier>
+        <source>https://pl.wikisource.org/wiki/Biblia_Wujka_(1923)</source>
+        <language type='IETF'>pl</language>
+        <rights>Public Domain</rights>
+        <refSystem>Bible</refSystem>
+      </work>
+    </header>
+  </osisText>
+</osis>'''
+
